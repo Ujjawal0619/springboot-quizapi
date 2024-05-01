e0bcb5a7469d7b1ebe93c403395df80176313c0f
ujjawal0619 <ujjawal.kumar0619@gmail.com>
2024-05-01 19:18:14 +0530

    added routes for question category and add question

--- src/main/java/com/ukdev/quizapp/Question.java
+++ src/main/java/com/ukdev/quizapp/Question.java
@@ -10,9 +10,10 @@ import lombok.Data;
 @Entity
 public class Question {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String questionTitle;
+    private String category;
     private String option1;
     private String option2;
     private String option3;
--- src/main/java/com/ukdev/quizapp/dao/QuestionDao.java
+++ src/main/java/com/ukdev/quizapp/dao/QuestionDao.java
@@ -4,7 +4,10 @@ import com.ukdev.quizapp.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
     // Extending JapRepository<ROM Class name/Table, PrimaryKey Type> help us to reduce few stuffs
+    List<Question> findByCategory(String category);
 }
